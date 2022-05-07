23f1fc04007d470a1e8fcdd26bec50dc1c2f47d5
Kevork <ksayad@gmail.com>
2022-05-06 23:06:22 -0400

    workflows

--- src/test/java/org/kevvlvl/cifriendly/lib/calculator/SimpleCalculatorTest.java
+++ src/test/java/org/kevvlvl/cifriendly/lib/calculator/SimpleCalculatorTest.java
@@ -1,6 +1,5 @@
 package org.kevvlvl.cifriendly.lib.calculator;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
